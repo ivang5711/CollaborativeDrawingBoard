64650419731265a0b3ea039aa2964ba24a40652a
ivang5711 <122921327+ivang5711@users.noreply.github.com>
2023-12-24 00:38:54 +0400

    Add project files.

---
+++ collaborativedrawingboard.client/src/App.jsx
@@ -0,0 +1,13 @@
+import Forms from './components/Forms';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="container">
+        <Forms />
+        </div>
+    );
+};
+
+export default App;
