943b50f3ba929026992ef6ca13928e24a82a4e21
ivang5711 <122921327+ivang5711@users.noreply.github.com>
2023-12-24 02:23:45 +0400

    Adds routing
    Set up routing.
    Added Whiteboard page.

--- collaborativedrawingboard.client/src/App.jsx
+++ collaborativedrawingboard.client/src/App.jsx
@@ -1,11 +1,17 @@
 import Forms from './components/Forms';
+import { Route, Routes } from "react-router-dom";
 
 import './App.css';
+
+import RoomPage from './pages/RoomPage';
 
 const App = () => {
     return (
         <div className="container">
-        <Forms />
+            <Routes>
+                <Route path="/" element={<Forms />} />
+                <Route path="/:roomId" element={ <RoomPage /> } />
+            </Routes>
         </div>
     );
 };
--- collaborativedrawingboard.client/src/main.jsx
+++ collaborativedrawingboard.client/src/main.jsx
@@ -4,9 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min"
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
 )
