0f5adcd7567441a580ace82e7e26667e61dabae2
ivang5711 <122921327+ivang5711@users.noreply.github.com>
2023-12-25 13:40:39 +0400

    Adds join board feature
    Added join bosrd feature.

--- collaborativedrawingboard.client/src/components/Forms/JoinRoomForm/index.jsx
+++ collaborativedrawingboard.client/src/components/Forms/JoinRoomForm/index.jsx
@@ -1,13 +1,43 @@
-const JoinRoomForm = () => {
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const JoinRoomForm = ({uuid, socket, setUser}) => {
+
+    const [roomId, setRoomId] = useState("");
+    const [name, setName] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleRoomJoin = (e) => {
+        e.preventDefault();
+
+        const roomData = {
+            name,
+            roomId,
+            userId: uuid(),
+            host: false,
+            presenter: false,
+        };
+        setUser(roomData);
+        navigate(`/${roomId}`);
+        socket.emit("userJoined", roomData);
+    }
+
     return (
         <form className="form col-md-12 mt-5">
             <div className="form-group">
-                <input type="text" className="form-control my-2" placeholder="Enter your name" />
+                <input type="text" className="form-control my-2" placeholder="Enter your name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}/>
             </div>
             <div className="form-group">
-                <input type="text" className="form-control my-2" placeholder="Enter room code" />
+                <input type="text" className="form-control my-2" placeholder="Enter room code"
+                value={roomId}
+                onChange={(e) => setRoomId(e.target.value)} />
             </div>
-            <button type="submit" className="mt-4 btn btn-primary btn-block form-control">
+            <button type="submit" className="mt-4 btn btn-primary btn-block form-control"
+                onClick={handleRoomJoin}
+            >
                 Join room
             </button>
         </form>
--- collaborativedrawingboard.client/src/components/Forms/index.jsx
+++ collaborativedrawingboard.client/src/components/Forms/index.jsx
@@ -11,7 +11,7 @@ const Forms = ({ uuid, socket, setUser }) => {
             </div>
             <div className="col-md-4 mt-5 form-box py-3 p-5 border border-2 border-secondary rounded-3 mx-auto d-flex flex-column align-items-center">
                 <h1 className="text-primary fw-bold">Join Room</h1>
-                <JoinRoomForm />
+                <JoinRoomForm uuid={uuid} socket={socket} setUser={setUser} />
             </div>
         </div>
     )
